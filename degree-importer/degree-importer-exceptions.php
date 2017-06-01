a6132c2a9ce557524b89bf551a7bebbc70a52c2d
Jim Barnes <james.barnes@ucf.edu>
2017-06-01 16:43:21 -0400

    Updated files to use proper indentation

--- degree-importer/degree-importer-exceptions.php
+++ degree-importer/degree-importer-exceptions.php
@@ -3,21 +3,21 @@
  * Defines the Degree_Importer_Exception class
  **/
 if ( ! class_exists( 'Search_Service_Exception' ) ) {
-	class Degree_Importer_Exception extends Exception {
-		/**
-		 * Creates a new instance of Degree_Importer_Exception
-		 * @author Jim Barnes
-		 * @since 1.0.0
-		 * @param $message string | The exception message
-		 * @param $code int | Default to 0
-		 * @param $previous Exception | The previous exception
-		 **/
-		public function __construct( $message, $code=0, Exception $previous=null ) {
-			parent::__construct( $message, $code, $previous );
-		}
+    class Degree_Importer_Exception extends Exception {
+        /**
+         * Creates a new instance of Degree_Importer_Exception
+         * @author Jim Barnes
+         * @since 1.0.0
+         * @param $message string | The exception message
+         * @param $code int | Default to 0
+         * @param $previous Exception | The previous exception
+         **/
+        public function __construct( $message, $code=0, Exception $previous=null ) {
+            parent::__construct( $message, $code, $previous );
+        }
 
-		public function __toString() {
-			return __CLASS__ . " : [$this->code}]: {$this->message}\n";
-		}
-	}
+        public function __toString() {
+            return __CLASS__ . " : [$this->code}]: {$this->message}\n";
+        }
+    }
 }
