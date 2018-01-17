91ec3e6c5927002aea90a2b442979e6afaa92629
Jo Dickson <jogreybill@gmail.com>
2018-01-17 14:24:21 -0600

    Removed the main site specific degree importer in favor of using the degree cpt plugin's importer; added hook for applying main site specific degree metadata to degrees during the import process

--- commands.php
+++ commands.php
@@ -11,11 +11,10 @@ if ( ! defined( 'WPINC' ) ) {
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	// Pull in the degree importer files.
-	require_once 'importers/degree-importer.php';
-	require_once 'importers/degree-importer-exceptions.php';
 	require_once 'importers/tuition-fees-importer.php';
 
 	require_once 'includes/degrees.php';
+	require_once 'includes/degree-import-overrides.php';
 
 	WP_CLI::add_command( 'mainsite degrees', 'Degrees' );
 
