46dbff3c8d81b9da293b0edccd09fb1254dd85e1
cadie <cadie@ucf.edu>
2022-06-14 15:14:18 -0400

    Add jobs shortcode and display logic

--- main-site-utilities.php
+++ main-site-utilities.php
@@ -15,6 +15,9 @@ if ( ! defined( 'WPINC' ) ) {
 define( 'UCF_MAIN_SITE_UTILITIES__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
 require_once UCF_MAIN_SITE_UTILITIES__PLUGIN_DIR . 'includes/config.php';
+require_once UCF_MAIN_SITE_UTILITIES__PLUGIN_DIR . 'includes/ucf-jobs-feed.php';
+require_once UCF_MAIN_SITE_UTILITIES__PLUGIN_DIR . 'includes/ucf-jobs-shortcode.php';
+
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	// Pull in the degree importer files.
 	require_once UCF_MAIN_SITE_UTILITIES__PLUGIN_DIR . 'importers/research-importer.php';
@@ -24,3 +27,7 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 
 	WP_CLI::add_command( 'research', 'UCF\MainSiteUtilities\Commands\ResearchCommands' );
 }
+
+add_action( 'plugins_loaded', function() {
+	add_action( 'init', array( __NAMESPACE__ . '\Shortcodes\Jobs_Shortcode', 'register_shortcode' ) );
+} );
