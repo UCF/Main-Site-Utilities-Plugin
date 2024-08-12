81ebf015e0d5aee47cebdb9742fede6df5e14851
Ramin Farhadi <ra680091@CMN9J7HFG4MN.attlocal.net>
2024-08-12 10:28:54 -0400

    Default url was updated.

--- includes/config.php
+++ includes/config.php
@@ -9,7 +9,6 @@ namespace UCF\MainSiteUtilities\Includes\Config;
 define( 'UCF_MAIN_SITE_UTILITIES__CUSTOMIZER_PREFIX', defined( 'THEME_CUSTOMIZER_PREFIX' ) ? THEME_CUSTOMIZER_PREFIX : 'ucf_main_site_' );
 
 define( 'UCF_MAIN_SITE_UTILITIES__CUSTOMIZER_DEFAULTS', serialize( array(
-	// TODO: These URLs will need to be updated once we have production URLs after July 1st
 	'jobs_base_url' => 'https://jobs.ucf.edu/jobs/search/',
 	'jobs_feed_url' => 'http://search.cm.ucf.edu/api/v1/positions/'
 ) ) );
